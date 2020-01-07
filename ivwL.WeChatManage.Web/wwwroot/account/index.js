a3cb9ec132fb94545e52e1602e6dd037513cd075
地沟油 <ljlivw@qq.com>
2020-01-07 14:18:39 +0800

    a

---
+++ ivwL.WeChatManage.Web/wwwroot/account/index.js
@@ -0,0 +1,34 @@
+﻿new Vue({
+    el: '#tplAccount',
+    template: '#tplAccount',
+    data: {
+        form: {
+            Code: '',
+            Password: ''
+        },
+        formSet: {
+            loginLoading: false,
+            txtLogin: '登陆'
+        }
+    },
+    methods: {
+        btnlogin() {
+            this.$data.formSet.loginLoading = true;
+            axios.post('/Account/Login', this.$data.form)
+                .then(res => {
+                    this.$data.formSet.loginLoading = false;
+                    if (res.data.code != 1) {
+                        this.$message.error({ showClose: true, message: res.data.errorMsg });
+                        return;
+                    }
+                    this.$message({ message: '登陆成功' });
+                    setTimeout(() => {
+                        window.location.href = '/Home/Index';
+                    }, 500);
+                }).catch(error => {
+                    this.$data.formSet.loginLoading = false;
+                    this.$alert(error);
+                })
+        }
+    }
+})
