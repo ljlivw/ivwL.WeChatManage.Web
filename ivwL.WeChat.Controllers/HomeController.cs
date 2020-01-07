a7efb8fa1aa37647a381c86f19184f6070d3d9e4
地沟油 <ljlivw@qq.com>
2020-01-07 11:29:32 +0800

    增加首页。

---
+++ ivwL.WeChat.Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ivwL.WeChat.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public async Task<IActionResult> Index() => await Task.Run(() => View());
+    }
+}
